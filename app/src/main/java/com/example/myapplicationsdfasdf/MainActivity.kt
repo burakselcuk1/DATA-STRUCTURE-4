2c8b6461f2de1bff9d7fbfa4c11179cb1ff57b9f
burakselcuk <burakselcuk1>
2023-01-18 12:24:20 +0300

    first commit by burak baba

---
+++ app/src/main/java/com/example/myapplicationsdfasdf/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myapplicationsdfasdf
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplicationsdfasdf.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.burak.text = "selcuk"
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplicationsdfasdf/test.kt
@@ -0,0 +1,17 @@
+package com.example.myapplicationsdfasdf
+
+fun main(){
+
+    var sayi = 2
+
+    var üst = 5
+
+    var sonuc = 1
+
+    for (i in 0 until üst){
+        sonuc *=sayi
+    }
+    println(sonuc)
+
+
+}
